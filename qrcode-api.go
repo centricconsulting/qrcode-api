93e6d05fbd401a02b0a21da33a67be877dd5cdc1
William Klos <wjklos@gmail.com>
2016-11-13 15:47:27 -0500

    Opened up CORS access controls.

--- qrcode-api.go
+++ qrcode-api.go
@@ -73,14 +73,14 @@ func init() {
 
 func cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		// c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		// c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		// c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-		// if c.Request.Method == "OPTIONS" {
-		// 	c.AbortWithStatus(204)
-		// 	return
-		// }
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
 		txn := app.StartTransaction(c.Request.URL.RequestURI(), c.Writer, c.Request)
 		defer txn.End()
 		c.Next()
